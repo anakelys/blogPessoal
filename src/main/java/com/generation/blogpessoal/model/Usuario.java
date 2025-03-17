fc8ccbbc33c0b21819dcfb6bd00608535cb4e43d
anakelys <souzanakely@gmail.com>
2025-03-17 14:49:57 -0300

    UsuarioControllerTest

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -43,6 +43,20 @@ public class Usuario {
 	@JsonIgnoreProperties("usuario")  //vai mostrar tudo que tem menos o usuario.
 	private List<Postagem> postagem;  //chave estrangeira
 
+	public Usuario(Long id,  String nome, String usuario,
+		String senha, String foto) {
+		
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+	
+	public Usuario() {
+
+	}
+
 	public Long getId() {
 		return id;
 	}
